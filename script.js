38115f02f1702ef2394689e8c15418897746229f
codingCat-dev <85181534+codingCat-dev@users.noreply.github.com>
2021-09-01 14:51:59 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -11,10 +11,29 @@ const navbarScrollingFunction = () => {
 };
 document.addEventListener('scroll', navbarScrollingFunction);
 
-const readMoreArticle = document.querySelector('.hidden-article');
-
 const form = document.querySelector('form');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
+});
+
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section-hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(section => {
+  sectionObserver.observe(section);
+  section.classList.add('section-hidden');
 });
